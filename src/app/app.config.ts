782290d4cea00f4aa79d6902654e063f56f36a56
Deepak Jha <deepakjha14.ex@gmail.com>
2024-04-05 20:38:52 +0530

    INstalled all the libraries and dependencies

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,9 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideAnimationsAsync(), provideStore()]
 };
